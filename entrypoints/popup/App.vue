c1ebdc2b523775708192d519ccd5a2a2aa61e146
miaobuao <miaobuao@outlook.com>
2024-12-25 20:03:09 +0800

    feat: auto focus

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useFocus } from '@vueuse/core'
 import DarkModeAdaptor from '@/components/DarkModeAdaptor.vue'
 import { Input } from '@/components/ui/input'
 import { Separator } from '@/components/ui/separator'
@@ -6,9 +7,15 @@ import { HistoryItem, useProcedure } from '@/utils/procedure'
 import ScrollArea from '@/components/ui/scroll-area/ScrollArea.vue'
 import { debounce } from 'lodash-es'
 
+const searchInput = ref<HTMLInputElement | null>(null)
+const { focused: searchInputFocused } = useFocus(searchInput)
 const searchString = ref('')
 const histories = ref<HistoryItem[]>([])
 const procedure = useProcedure()
+
+onMounted(() => {
+	searchInputFocused.value = true
+})
 
 const search = debounce(async function () {
 	if (!(await procedure.hasIndex())) {
@@ -27,7 +34,7 @@ function open(url: string) {
 	<DarkModeAdaptor />
 	<div class="flex flex-col w-72 pt-2 px-2 gap-2">
 		<div class="flex items-center gap-2">
-			<Input v-model="searchString" @keydown="search" />
+			<Input ref="searchInput" v-model="searchString" @keydown="search" />
 		</div>
 		<ScrollArea class="h-72 p-1">
 			<template v-for="item in histories" :key="item.id">
