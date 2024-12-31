1f7335d5a631c6fde67df813dfd86d5b00c1b513
miaobuao <miaobuao@outlook.com>
2024-12-31 17:42:50 +0800

    feat: loading animation

--- entrypoints/popup/App.vue
+++ entrypoints/popup/App.vue
@@ -6,23 +6,30 @@ import { Separator } from '@/components/ui/separator'
 import { HistoryItem, useProcedure } from '@/utils/procedure'
 import { debounce } from 'lodash-es'
 import HistoryItemComponent from '@/components/HistoryItem.vue'
+import { Loader2 } from 'lucide-vue-next'
 
 const searchInput = ref<HTMLInputElement | null>(null)
 const { focused: searchInputFocused } = useFocus(searchInput)
 const searchString = ref('')
 const histories = ref<HistoryItem[]>([])
 const procedure = useProcedure()
+const loadingSearchResult = ref(false)
 
 onMounted(() => {
 	searchInputFocused.value = true
 })
 
 const search = debounce(async function () {
-	if (!(await procedure.hasIndex())) {
-		await procedure.initFuseIndexing()
+	loadingSearchResult.value = true
+	try {
+		if (!(await procedure.hasIndex())) {
+			await procedure.initFuseIndexing()
+		}
+		const res = await procedure.search(searchString.value)
+		histories.value = res.map((d) => d.item)
+	} finally {
+		loadingSearchResult.value = false
 	}
-	const res = await procedure.search(searchString.value)
-	histories.value = res.map((d) => d.item)
 }, 300)
 
 function open(url: string) {
@@ -32,11 +39,22 @@ function open(url: string) {
 
 <template>
 	<DarkModeAdaptor />
-	<div class="flex flex-col w-72 p-2 gap-2">
+	<div class="flex flex-col w-72 h-96 px-2 pt-2 gap-2">
 		<div class="flex items-center gap-2">
 			<Input ref="searchInput" v-model="searchString" @keydown="search" />
 		</div>
-		<DynamicScroller class="h-72 px-1" :items="histories" :min-item-size="50">
+		<div
+			v-show="loadingSearchResult"
+			class="flex-1 flex justify-center items-center"
+		>
+			<Loader2 class="size-8 animate-spin" />
+		</div>
+		<DynamicScroller
+			v-show="!loadingSearchResult"
+			class="flex-1 px-1"
+			:items="histories"
+			:min-item-size="50"
+		>
 			<template v-slot="{ item, index, active }">
 				<DynamicScrollerItem
 					:item="item"
@@ -47,7 +65,7 @@ function open(url: string) {
 					<div class="cursor-pointer py-2" @click="open(item.url!)">
 						<HistoryItemComponent :history="item" />
 					</div>
-					<Separator />
+					<Separator v-if="index !== histories.length - 1" />
 				</DynamicScrollerItem>
 			</template>
 		</DynamicScroller>
